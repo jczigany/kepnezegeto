2b20233bdc3ecce53d112b6b39556801fcc3c101
Czigány János <jczigany@infomagus.hu>
2020-03-03 18:24:27 +0100

    Image file-ok kilistázása a kiválasztott folder-ből

--- FirstApp.py
+++ FirstApp.py
@@ -1,6 +1,7 @@
-from PySide2.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QListWidget, QTextEdit, QLabel
-
-import sys
+from PySide2.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QListWidget, QTextEdit, \
+    QLabel, QFileDialog
+from PySide2.QtCore import QObject, SIGNAL
+import sys, os
 
 
 class PhotoViewer(QWidget):
@@ -8,6 +9,10 @@ class PhotoViewer(QWidget):
         super(PhotoViewer, self).__init__()
         self.setWindowTitle("Képnézegető")
         self.resize(1000, 600)
+
+        # objektum változók
+        self.current_dir = ""
+        self.file_list = []
 
         # Main layout with "Open folder" button
         main_layout = QVBoxLayout(self)
@@ -39,6 +44,21 @@ class PhotoViewer(QWidget):
         self.image_viewer = QLabel("Itt lesz a kép")
         h_layout.addWidget(self.image_viewer)
 
+        # Connect signals
+        self.open_button.clicked.connect(self.open_folder_action)
+
+
+    def collect_files(self):
+        self.file_list = [i for i in os.listdir(self.current_dir) if i.lower().endswith(".jpg")]
+        for i in self.file_list:
+            print(i)
+
+    def open_folder_action(self):
+        directory = QFileDialog.getExistingDirectory(self, "Könyvtár választás", "C:\\Users\\jcigi\\Pictures\\gdc")
+
+        if len(directory):
+            self.current_dir = directory
+            self.collect_files()
 
 app = QApplication(sys.argv)
 win = PhotoViewer()
