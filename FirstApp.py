93328561b9d9a176f2cf49d7f76a358b9a49b72f
Czigány János <jczigany@infomagus.hu>
2020-03-04 11:10:23 +0100

    Alapfunkciók kész

--- FirstApp.py
+++ FirstApp.py
@@ -1,6 +1,6 @@
 from PySide2.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QListWidget, QTextEdit, \
     QLabel, QFileDialog
-from PySide2.QtCore import QObject, SIGNAL
+from PIL import Image, ExifTags
 import sys, os
 
 
@@ -46,19 +46,61 @@ class PhotoViewer(QWidget):
 
         # Connect signals
         self.open_button.clicked.connect(self.open_folder_action)
+        self.file_list_view.currentItemChanged.connect(self.photo_changed_action)
+        self.file_list_view.itemDoubleClicked.connect(self.open_file_action)
 
+    def getExif(self, filePath):
+        exif_string = ""
+
+        exif_string += f"File: {filePath}\n"
+
+        img = Image.open(filePath)
+        exif = img._getexif()
+        if exif:
+            for k, v in exif.items():
+                if k in ExifTags.TAGS:
+                    exifLabel = ExifTags.TAGS[k]
+
+                    if exifLabel == "DateTimeOriginal":
+                        exif_string += f"Date: {v}\n"
+
+                    if exifLabel == "Model":
+                        exif_string += f"Camera: {v}\n"
+
+                    if exifLabel == "ISOSpeedRatings":
+                        exif_string += f"ISO: {v}\n"
+
+        return exif_string
+
+    def open_file_action(self, item):
+        os.startfile(os.path.join(self.current_dir, item.text()))
+
+    def photo_changed_action(self, item):
+        # print(item.text())
+        current_photo = os.path.join(self.current_dir, item.text())
+        exif_data = self.getExif(current_photo)
+        self.photo_details.setText(exif_data)
+
+        #self.image_viewer.set_pixmap(current_photo)
+        self.image_viewer.setPixmap(current_photo)
+
+    def refresh_file_list_view(self):
+        self.file_list_view.clear()
+        for f in self.file_list:
+            self.file_list_view.addItem(f)
 
     def collect_files(self):
         self.file_list = [i for i in os.listdir(self.current_dir) if i.lower().endswith(".jpg")]
-        for i in self.file_list:
-            print(i)
 
     def open_folder_action(self):
         directory = QFileDialog.getExistingDirectory(self, "Könyvtár választás", "C:\\Users\\jcigi\\Pictures\\gdc")
 
         if len(directory):
-            self.current_dir = directory
+            self.current_dir = directory.replace("/", "\\")
+            self.open_button.setText(self.current_dir)
             self.collect_files()
+            self.refresh_file_list_view()
+
 
 app = QApplication(sys.argv)
 win = PhotoViewer()
